4f5effd63884c8e37b54e7bd83efad3ac68cadd5
Paweł Rapkiewicz <pawel.rapkiewicz@gmail.com>
2023-09-16 18:25:39 +0000

    Split bootstrap and setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,73 +1,64 @@
 #!/bin/bash
 
-if [[ $EUID -eq 0 ]]; then
-  echo "This script must NOT be run as root, nor with sudo."
-  exit 1
-fi
-
-username=$(logname)
-
-package_list="htop tmux zsh unzip"
-echo "Will install the following packages '${package_list}'. Please pass a root password for apt command"
-
-sudo -- sh -c "apt update && apt install -y ${package_list}"
-if [[ $? -ne 0 ]]; then
-  echo "Something went wrong $?"
-  exit 2
-fi
-
-current_shell=$(getent passwd "$USER" | cut -d: -f7)
-if [ "$current_shell" != "$(which zsh)" ]; then
-  # Change the user's default shell to Zsh
-  echo "Changing default shell to zsh"
-  chsh -s $(which zsh) $username
-  echo "Changed the default shell for user $username to Zsh."
-else
-  echo "User $username already has Zsh as their default shell."
-fi
-
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  echo "Installing Oh-My-Zsh..."
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-fi
-
-PLUGINS_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
-if [ ! -d "$PLUGINS_CUSTOM/plugins/zsh-autosuggestions" ]; then
-  echo "Installing zsh-autosuggestions plugin..."
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${PLUGINS_CUSTOM}/plugins/zsh-autosuggestions
-fi
-
-if [ ! -d "$PLUGINS_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
-  echo "Installing zsh-syntex-highlighting..."
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${PLUGINS_CUSTOM}/plugins/zsh-syntax-highlighting
-fi
-
-echo "Cloning dotfiles..."
-git clone https://github.com/vincentto13/dotfiles.git $HOME/.dotfiles
-
-for file in $HOME/.dotfiles/.*; do
-  if [ -f $file ]; then
-    filename=$(basename $file)
-    makebackup=false
-    if [ -L $HOME/$filename ]; then
-      echo "Symbolic link"
-      pointto=$(realpath $(readlink -- $HOME/$filename))
-      if [ "$pointto" == "$file" ]; then
-        echo "Symlink correect skip"
-        continue
-      fi
-      makebackup=true
-    elif [ -f $HOME/$filename ]; then
-      echo "Regular file"
-      makebackup=true
-    fi
-    if [ "$makebackup" == true ]; then
-      echo "making backup"
-      mv $HOME/$filename $HOME/$filename.bak
-    fi
-    ln -s $file $HOME
-    echo $file
+# Function to install Homebrew on macOS if not already installed
+install_homebrew() {
+  if ! command -v brew &>/dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
-done
-#echo "Installing NeoVim"
-#wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
+}
+
+# Function to install Git and clone/update the dotfiles repository
+install_git_and_clone_dotfiles() {
+  # Check if Git is installed
+  if ! command -v git &>/dev/null; then
+    echo "Git is not installed. Installing Git..."
+    case "$(uname -s)" in
+      Darwin)
+        # macOS using Homebrew
+        install_homebrew
+        brew install git
+        ;;
+      Linux)
+        # Linux using package manager (Debian/Ubuntu, Arch, Alpine, SLES, RedHat)
+        if command -v apt &>/dev/null; then
+          sudo apt update
+          sudo apt install -y git
+        elif command -v pacman &>/dev/null; then
+          sudo pacman -Syu --noconfirm git
+        elif command -v apk &>/dev/null; then
+          sudo apk update
+          sudo apk add git
+        elif command -v zypper &>/dev/null; then
+          sudo zypper --non-interactive install git
+        elif command -v yum &>/dev/null; then
+          sudo yum install -y git
+        else
+          echo "Unsupported Linux distribution. Please install Git manually."
+          exit 1
+        fi
+        ;;
+      *)
+        echo "Unsupported operating system: $(uname -s)"
+        exit 1
+        ;;
+    esac
+  fi
+
+  # Check if $HOME/.dotfiles exists
+  if [ -d "$HOME/.dotfiles" ]; then
+    echo "Updating existing dotfiles repository..."
+    cd "$HOME/.dotfiles" || exit
+    git pull
+  else
+    # Clone the dotfiles repository into $HOME/.dotfiles
+    git clone https://github.com/vincentto13/dotfiles "$HOME/.dotfiles"
+    cd "$HOME/.dotfiles" || exit
+  fi
+
+  # Run the setup script
+  ./setup.sh
+}
+
+# Run the installation function
+install_git_and_clone_dotfiles
+
